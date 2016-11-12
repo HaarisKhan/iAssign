a0335b1b90a01b49369e9cd82b20c723782fadd7
Haaris Khan <haariskhan902@gmail.com>
2016-11-12 02:49:26 -0800

    Created basic login system, templates, models, etc.

--- iAssign/app/urls.py
+++ iAssign/app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
+    url(r'^login$', views.login_view, name='login'),
+    url(r'^logout$', views.logout_view, name='logout'),
+    url(r'^messages$', views.message, name='message')
 ]
--- iAssign/app/views.py
+++ iAssign/app/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, render_to_response
+from django.template.context_processors import csrf
+from django.urls import reverse
+
+from . import models
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def login_view(request):
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return render(request, 'main/login.html')
+
+    return render_to_response('main/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+
+
+def message(request):
+    c = models.Chat.objects.all()
+    return render(request, 'main/messages.html', {'chat': c})
