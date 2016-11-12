12d58e49f415eef80ee81bdbc979ff5dea93ac7f
girishbalaji <gbalaji10@gmail.com>
2016-11-12 12:51:23 -0800

    models should be done

--- iAssign/app/models.py
+++ iAssign/app/models.py
@@ -13,6 +13,13 @@ class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     boards = models.ManyToManyField(Board)
+    email = models.CharField(max_length=250)
+
+
+class Organization(models.Model):
+    users = models.ManyToManyField(Person)
+    boards = models.OneToManyField(Board)
+    org_name = models.CharField(max_length=250)
 
 
 class Board(models.Model):
@@ -20,15 +27,24 @@ class Board(models.Model):
     task = models.CharField(max_length=250)
     num_people = models.IntegerField()
     moderator = models.ManyToManyField(Person)
-    times = models.ManyToManyField(TimeSlot)
-    # requests = models.ForeignKey(Requests)
+    times = models.ManyToManyField(TimeIntervalObject)
+    requests = models.ForeignKey(Requests)
 
 
-class TimeSlot(models.Model):
+class TimeIntervalObject(models.Model):
     boards = models.ManyToManyField(Board)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    people = models.ManyToManyField(Person)
 
 
 class Requests(models.Model):
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
-    #times = models.
+    original_time = models.OneToOneField(TimeIntervalObject)
+    original_person = models.OneToOneField(Person)
+    end_time = models.OneToOneField(TimeIntervalObject)
+    end_person = models.OneToOneField(Person)
+    original_person_approval = models.BooleanField
+    end_person_approval = models.BooleanField
+
 
