e71ee73c124efb5e7071670907ca3cac6298d41d
Ally Koo <allysonkoo@berkeley.edu>
2016-11-12 04:34:57 -0800

    Added models, still unsure about many to many relationships

--- iAssign/app/models.py
+++ iAssign/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from . import fields
 
 # Create your models here.
 
@@ -9,3 +10,25 @@ class Chat(models.Model):
     create = models.DateTimeField(auto_now_add=True)
     message = models.CharField(max_length=250)
 
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    boards = models.ManyToManyField(Board)
+
+
+class Board(models.Model):
+    people = models.ManyToManyField(Person)
+    task = models.CharField(max_length=250)
+    num_people = models.IntegerField()
+    moderator = models.ManyToOneField(Person)
+    times = fields.ListField()
+    requests = fields.ListField()
+
+
+class TimeSlot(models.Model):
+    boards = fields.ListField()
+
+
+class Requests(models.Model):
+    times = fields.ListField()
