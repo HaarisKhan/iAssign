84746a542c7ed93c23791d2f65f3f2609a9b0172
Ally Koo <allysonkoo@berkeley.edu>
2016-11-13 02:44:08 -0800

    Trying to make authentication work for everyone pls work i'm so tired

--- iAssign/app/views.py
+++ iAssign/app/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 
 import httplib2
 import os
+#
+#from quickstart import get_credentials
 
 from apiclient import discovery
 from oauth2client import client
@@ -36,6 +38,50 @@ def ThirdAuthLogin(request):
                                               'user': request.user})
 
 
+# def get_credentials():
+#     """Gets valid user credentials from storage.
+#
+#     If nothing has been stored, or if the stored credentials are invalid,
+#     the OAuth2 flow is completed to obtain the new credentials.
+#
+#     Returns:
+#         Credentials, the obtained credential.
+#     """
+#     # If modifying these scopes, delete your previously saved credentials
+#     # at ~/.credentials/calendar-python-quickstart.json
+#     SCOPES = 'https://www.googleapis.com/auth/calendar'
+#     CLIENT_SECRET_FILE = 'client_secret.json'
+#     APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+#     home_dir = os.path.expanduser('~')
+#     credential_dir = os.path.join(home_dir, '.credentials')
+#     if not os.path.exists(credential_dir):
+#         os.makedirs(credential_dir)
+#     credential_path = os.path.join(credential_dir,
+#                                    'calendar-python-quickstart.json')
+#
+#     store = Storage(credential_path)
+#     credentials = store.get()
+#     if not credentials or credentials.invalid:
+#         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+#         flow.user_agent = APPLICATION_NAME
+#         # Needed only for compatibility with Python 2.6
+#         credentials = tools.run(flow, store)
+#     return credentials
+
+try:
+    import argparse
+
+    flags = tools.argparser.parse_args([])
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/calendar-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+
+
 def get_credentials():
     """Gets valid user credentials from storage.
 
@@ -45,11 +91,6 @@ def get_credentials():
     Returns:
         Credentials, the obtained credential.
     """
-    # If modifying these scopes, delete your previously saved credentials
-    # at ~/.credentials/calendar-python-quickstart.json
-    SCOPES = 'https://www.googleapis.com/auth/calendar'
-    CLIENT_SECRET_FILE = 'client_secret.json'
-    APPLICATION_NAME = 'Google Calendar API Python Quickstart'
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
@@ -64,13 +105,15 @@ def get_credentials():
         flow.user_agent = APPLICATION_NAME
         if flags:
             credentials = tools.run_flow(flow, store, flags)
-        else:  # Needed only for compatibility with Python 2.6
+        else: # Needed only for compatibility with Python 2.6
             credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
     return credentials
 
 
 def DisplayCalendar(request):
 
+    #credentials = get_credentials()
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
@@ -83,9 +126,6 @@ def DisplayCalendar(request):
     return render(request, "appPage.html", {'request': request,
                                          'user': request.user,
                                          'calendar_display': calendar_display})
-
-
-
 
 
 def Login(request):
