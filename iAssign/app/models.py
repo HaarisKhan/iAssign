239b9ea723013d06dcbd1c93a683606f9c818e26
Haaris Khan <haariskhan902@gmail.com>
2016-11-12 17:15:22 -0800

    Forced commit

--- iAssign/app/models.py
+++ iAssign/app/models.py
@@ -2,20 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User, AbstractUser
 
 
-#from . import fields
-
-
-class Chat(models.Model):
-    username = models.ForeignKey(User)
-    create = models.DateTimeField(auto_now_add=True)
-    message = models.CharField(max_length=250)
-
-
-"""
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     boards = models.ManyToManyField(Board)
+    email = models.CharField(max_length=250)
+
+
+class Organization(models.Model):
+    users = models.ManyToManyField(Person)
+    boards = models.ForeignKey(Board, on_delete=models.CASCADE)
+    org_name = models.CharField(max_length=250)
 
 
 class Board(models.Model):
@@ -23,15 +20,26 @@ class Board(models.Model):
     task = models.CharField(max_length=250)
     num_people = models.IntegerField()
     moderator = models.ManyToManyField(Person)
-    times = models.ManyToManyField(TimeSlot)
-    # requests = models.ForeignKey(Requests)
+    times = models.ForeignKey(TimeIntervalObject, on_delete=models.CASCADE)
+    requests = models.ForeignKey(Request)
+
+    # Create new time interval
+    def Create_Time_Interval(self):
+        return None
 
 
-class TimeSlot(models.Model):
-    boards = models.ManyToManyField(Board)
+class TimeIntervalObject(models.Model):
+    boards = models.OneToOneField(Board)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    people = models.ManyToManyField(Person)
 
 
-class Requests(models.Model):
+class Request(models.Model):
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
-    #times = models.
-"""
+    original_time = models.OneToOneField(TimeIntervalObject)
+    original_person = models.OneToOneField(Person)
+    end_time = models.OneToOneField(TimeIntervalObject)
+    end_person = models.OneToOneField(Person)
+    original_person_approval = models.BooleanField()
+    end_person_approval = models.BooleanField()
