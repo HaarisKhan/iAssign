ce315275a11e1a742fd9a55686428915465b4654
Ally Koo <allysonkoo@berkeley.edu>
2016-11-13 03:45:11 -0800

    Saving changes to merge

--- iAssign/app/views.py
+++ iAssign/app/views.py
@@ -38,36 +38,6 @@ def ThirdAuthLogin(request):
                                               'user': request.user})
 
 
-# def get_credentials():
-#     """Gets valid user credentials from storage.
-#
-#     If nothing has been stored, or if the stored credentials are invalid,
-#     the OAuth2 flow is completed to obtain the new credentials.
-#
-#     Returns:
-#         Credentials, the obtained credential.
-#     """
-#     # If modifying these scopes, delete your previously saved credentials
-#     # at ~/.credentials/calendar-python-quickstart.json
-#     SCOPES = 'https://www.googleapis.com/auth/calendar'
-#     CLIENT_SECRET_FILE = 'client_secret.json'
-#     APPLICATION_NAME = 'Google Calendar API Python Quickstart'
-#     home_dir = os.path.expanduser('~')
-#     credential_dir = os.path.join(home_dir, '.credentials')
-#     if not os.path.exists(credential_dir):
-#         os.makedirs(credential_dir)
-#     credential_path = os.path.join(credential_dir,
-#                                    'calendar-python-quickstart.json')
-#
-#     store = Storage(credential_path)
-#     credentials = store.get()
-#     if not credentials or credentials.invalid:
-#         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-#         flow.user_agent = APPLICATION_NAME
-#         # Needed only for compatibility with Python 2.6
-#         credentials = tools.run(flow, store)
-#     return credentials
-
 try:
     import argparse
 
